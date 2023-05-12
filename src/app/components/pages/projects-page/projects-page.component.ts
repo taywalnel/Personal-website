8c64daed9ab6fb37e8f2153751c66942268f947f
Taylor Nelson <taylornelson@Taylors-Laptop.local>
2023-05-12 15:59:42 +1200

    [story] add animation to projects page

--- src/app/components/pages/projects-page/projects-page.component.ts
+++ src/app/components/pages/projects-page/projects-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'app-projects-page',
   templateUrl: './projects-page.component.html',
-  styleUrls: ['./projects-page.component.scss']
+  styleUrls: ['./projects-page.component.scss'],
 })
 export class ProjectsPageComponent {
-
+  constructor(public app: AppComponent) {}
 }
