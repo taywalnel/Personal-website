368e5a4e8b0807101bdc68dfc58d76a2e9935824
Taylor Nelson <taylornelson@Taylors-Laptop.fritz.box>
2023-05-14 15:55:09 +1200

    [tweak] remove unsued constructor and ngOnInit

--- src/app/components/pages/skills-page/skills-page.component.ts
+++ src/app/components/pages/skills-page/skills-page.component.ts
@@ -6,68 +6,8 @@ import { AppComponent } from 'src/app/app.component';
   templateUrl: './skills-page.component.html',
   styleUrls: ['./skills-page.component.scss'],
 })
-export class SkillsPageComponent implements OnInit {
+export class SkillsPageComponent {
   triggerAnimation = false;
-  // skills = {
-  //   frontend: [
-  //     {
-  //       label: 'Angular',
-  //       rating: '90%',
-  //     },
-  //     {
-  //       label: 'React',
-  //       rating: '50%',
-  //     },
-  //     {
-  //       label: 'JavaScript',
-  //       rating: '80%',
-  //     },
-  //     {
-  //       label: 'TypeScript',
-  //       rating: '90%',
-  //     },
-  //     {
-  //       label: 'HTML',
-  //       rating: '90%',
-  //     },
-  //     {
-  //       label: 'CSS',
-  //       rating: '90%',
-  //     },
-  //   ],
-  //   backend: [
-  //     {
-  //       label: 'NodeJS',
-  //       rating: '70%',
-  //     },
-  //     {
-  //       label: 'MongoDB',
-  //       rating: '50%',
-  //     },
-  //     {
-  //       label: 'ExpressJS',
-  //       rating: '50%',
-  //     },
-  //   ],
-  //   additional: [
-  //     {
-  //       label: 'Photoshop',
-  //       rating: '70%',
-  //     },
-  //     {
-  //       label: 'Figma',
-  //       rating: '60%',
-  //     },
-  //     {
-  //       label: 'AWS',
-  //       rating: '60%',
-  //     },
-  //     {
-  //       label: 'RxJS',
-  //       rating: '80%',
-  //     },
-  //   ],
-  // };
   skillsConfig = [
     {
       label: 'JavaScript',
@@ -119,11 +59,4 @@ export class SkillsPageComponent implements OnInit {
       iconType: 'photoshop',
     },
   ];
-  constructor(private app: AppComponent) {}
-
-  ngOnInit() {
-    this.app.currentPage$.subscribe((currentPage) => {
-      this.triggerAnimation = currentPage === 'skills-page';
-    });
-  }
 }
